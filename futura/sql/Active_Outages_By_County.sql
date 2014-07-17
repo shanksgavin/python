916c68f14855ccc7c7a636b5b82ca4c31ec74c20
shanksgavin <shanksgavin@gmail.com>
2014-07-17 15:14:53 -0400

    second revision

--- futura/sql/Active_Outages_By_County.sql
+++ futura/sql/Active_Outages_By_County.sql
@@ -1,16 +1,7 @@
-select distinct(m.county) as county, 
-    count(cc.customer), 
-    cc.deleted as cc_del,
-    c.deleted as c_del
-    --m.elementid, 
-    --m.county 
-from casescustomers cc, meterbase m, cases c
-where cc.customer = m.elementid 
-AND cc.casenum = c.casenum
-AND c.casestatus in ('CauseFound', 'CauseUnknown', 'Predicted')
-AND cc.deleted = FALSE
---AND c.deleted = FALSE
---AND (c.datestrt>='2013-01-24' and c.dateend<='2013-03-04')
-GROUP BY county, cc.deleted, c.deleted
---GROUP BY customers.county
-ORDER BY county ASC
+select c.elementid, c.element, c.casenum, c.custout, c.casestatus, c.status, c.elementname, c.feeder, c.deleted
+from cases as c
+where c.casestatus in ('CauseFound', 'CauseUnknown', 'Predicted')
+and c.deleted = FALSE
+UNION
+select cb.elementid, cb.element, cb.casenum, cb.custout, cb.casestatus, cb.status, cb.elementname, cb.feeder, cb.deleted
+from callbundles as cb
