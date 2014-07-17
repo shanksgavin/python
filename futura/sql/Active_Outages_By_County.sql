27b847d5368fba4925649857f2b705f3c4c5eee9
shanksgavin <shanksgavin@gmail.com>
2014-07-17 15:15:37 -0400

    third revision

--- futura/sql/Active_Outages_By_County.sql
+++ futura/sql/Active_Outages_By_County.sql
@@ -1,6 +1,8 @@
 select c.elementid, c.element, c.casenum, c.custout, c.casestatus, c.status, c.elementname, c.feeder, c.deleted
-from cases as c
-where c.casestatus in ('CauseFound', 'CauseUnknown', 'Predicted')
+from cases as c, casescustomers as cc, meterbase as m 
+where c.casenum = cc.casenum
+and cc.customer = m.elementid
+and c.casestatus in ('CauseFound', 'CauseUnknown', 'Predicted')
 and c.deleted = FALSE
 UNION
 select cb.elementid, cb.element, cb.casenum, cb.custout, cb.casestatus, cb.status, cb.elementname, cb.feeder, cb.deleted
