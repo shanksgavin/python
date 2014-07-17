7e825f17e61f4fe6877456ef455587c714ea90f8
shanksgavin <shanksgavin@gmail.com>
2014-07-17 15:14:02 -0400

    first revision

--- futura/sql/Active_Outages_By_County.sql
+++ futura/sql/Active_Outages_By_County.sql
@@ -1,10 +1,16 @@
-﻿SELECT DISTINCT(county) as county, COUNT(county) as outages from (
-select cc.customer, m.elementid, m.county from casescustomers cc, meterbase m, cases c
+select distinct(m.county) as county, 
+    count(cc.customer), 
+    cc.deleted as cc_del,
+    c.deleted as c_del
+    --m.elementid, 
+    --m.county 
+from casescustomers cc, meterbase m, cases c
 where cc.customer = m.elementid 
 AND cc.casenum = c.casenum
 AND c.casestatus in ('CauseFound', 'CauseUnknown', 'Predicted')
 AND cc.deleted = FALSE
+--AND c.deleted = FALSE
 --AND (c.datestrt>='2013-01-24' and c.dateend<='2013-03-04')
-) AS customers
-GROUP BY customers.county
-ORDER BY customers.county ASC
+GROUP BY county, cc.deleted, c.deleted
+--GROUP BY customers.county
+ORDER BY county ASC
